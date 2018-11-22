7d44d091449f01ac4d2141ea47e52cc26c595d5d
nyoki-mtl <charmer.popopo@gmail.com>
2018-11-22 20:07:18 +0900

    Modify cityscapes

--- src/losses/multi/ce_loss.py
+++ src/losses/multi/ce_loss.py
@@ -9,7 +9,7 @@ class CrossEntropy2d(nn.Module):
         self.weight = weight
         self.size_average = size_average
         self.batch_average = batch_average
-        self.loss_fn = nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_index, size_average=False)
+        self.loss_fn = nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_index, reduction='sum')
 
     def forward(self, preds, labels):
         n, c, h, w = preds.size()
@@ -19,3 +19,32 @@ class CrossEntropy2d(nn.Module):
         if self.batch_average:
             loss /= n
         return loss
+
+
+class OhemCrossEntropy2d(nn.Module):
+    def __init__(self, ignore_label=255, weight=None, thresh=0.7):
+        super().__init__()
+        self.ignore_label = ignore_label
+        self.weight = weight
+        self.thresh = thresh
+        self.loss_fn = nn.NLLLoss()
+        self.softmax = nn.LogSoftmax()
+
+    def forward(self, preds, labels):
+        # n, c, h, w = preds.size()
+        preds = self.softmax(preds)
+
+        loss = torch.Tensor(1).zero_()
+        for idx, pred in enumerate(preds):
+            pred = pred.unsqueeze(0)
+            label = labels[idx]
+            loss = torch.cat([loss, self.loss_fn(pred, label)], 0)
+
+        loss = loss[1:]
+        # if self.thresh == 1:
+        #     valid_loss = loss
+
+        index = torch.topk(loss, int(self.thresh * loss.size()[0]))
+        valid_loss = loss[index[1]]
+
+        return torch.mean(valid_loss)
